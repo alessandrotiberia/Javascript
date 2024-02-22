51c2e084382896c9998c1f91becb995ff1b4f593
alessandrotiberia <150442623+alessandrotiberia@users.noreply.github.com>
2024-02-22 21:57:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Ciao mondo");
+console.log("Hello World");
+/* Valori primitivi:
+1 Number: usati valori decimali e interi
+2 String: usata per il testo
+3 Boolean: Valore vero o falso per prendere decisioni
+4 Undefined: valore nullo non previsto
+5 null: valore volutamente nullo
+6 symbol: valore unico non può essere cambiato
+7 BygInt: valori più grandi di Number
+*/
+let a= "ciao";
+const b= "hello"; // non può essere cambiata
+a= "hola"; // con let può essere cambiata
+console.log(a);
+//var c= "Monkey"; 
+
+    // OPERATORI ARITMETICI JAVASCRIPT
+    let esponente = 2 **2 ; // 4
+    let divisione= 8 / 2 ; // 4
+    let moltiplicazione = 8 * 2; // 16
+    let sottrazione = 9 - 1 ; // 8
+    let addizione = 7 + 1; // 8
+const Nome= "Ale";
+const messaggio= "ciao" + Nome;
+console.log(messaggio);
